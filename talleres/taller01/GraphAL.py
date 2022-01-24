1b2dc07b82da491cdc14c7856b50080fa72e9d2f
SaraCastril1 <84990901+SaraCastril1@users.noreply.github.com>
2022-01-24 12:07:07 -0500

    Add files via upload

--- talleres/taller01/GraphAL.py
+++ talleres/taller01/GraphAL.py
@@ -1,16 +1,29 @@
+import numpy as np
+from collections import deque #Libreria para Listas
+
 class GraphAL:
     def __init__(self, size):
-
+      self.arregloDeLista = [0]*size
+      for i in range(0,size):
+         arregloDeLista[i] = deque()
 
     def addArc(self, vertex, edge, weight):
+        fila = self.arregloDeLista[vertex]
+        parejaDestinoPeso = (destination, weight)
+        fila.append(parejaDestinoPeso)  #appendleft para añadir al inicio.
 
 
     def getSuccessors(self, vertice):
+        filaVertice = self.arregloDeLista[vertice]
+      respuesta = []
+      
+      for j in range(0,size):
+        if filaVertice[j] != 0:
+           respuesta.append(j)
+      return respuesta
 
 
     def getWeight(self, source, destination):
-
-
-    def __str__(self):
+        return self.arregloDeLista[source][destination]
 
 
