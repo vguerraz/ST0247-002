3b65edf33c4ad64393e1423eb39aacfe34a6c97d
SaraCastril1 <84990901+SaraCastril1@users.noreply.github.com>
2022-02-14 09:36:59 -0500

    Update GraphAL.py

--- talleres/taller01/GraphAL.py
+++ talleres/taller01/GraphAL.py
@@ -1,30 +1,45 @@
 from collections import deque
 
-class Nodo:
-    def __init__(self, valor, next = None):
-        self.valor = valor
-        self.next = next
-
 class GraphAL:
     def __init__(self, size):
-        self.arregloDeListas = [0]*size
-        for i in range(0,size):
-            self.arregloDeListas[i] = deque()
-            
-    def esta_vacia(self):
-        return self.head == None #CONDICIONAL DEVUELVE BOOLEAN
+      self.arregloDeListas = [0]*size
+        #[size]
+        #[ [], [], [], [] , [] ,[] ...]
+
+      for i in range(0, size):
+        self.arregloDeListas[i]= deque()
+  
         
     def addArc(self, vertex, destination, weight):
-        fila = self.arregloDeListas[vertex]
-        parejaDestinoPeso = (destination, weight)
-        fila.append(parejaDestinoPeso)
+       fila = self.arregloDeListas[vertex-1]
+       arco = (destination,weight)
+       fila.append(arco)
         
+    def getWeight(self, source, destination):   
+      return self.arregloDeListas[source-1][destination-1] #Solo la segunda Componente
+      
+
     def getSuccessors(self, vertice):
         for i in range(0,vertice):
             successors = self.arregloDeListas
             return successors
-            
-    def getWeight(self, source, destination):
-        for i in range(0,destination):
-            weight = self.arregloDeListas
-            return weight
+       
+
+
+size = int(input("Número de vértices del grafo:"))
+n_edges =int(input("Número de aristas del grafo:")) 
+graph = GraphAL(size)
+print(graph.arregloDeListas)
+
+
+for i in range (n_edges):
+  source = int(input("Origen: ")) 
+  destination = int(input("Destino: "))
+  weight = int(input("Peso: "))
+
+  graph.addArc(source, destination, weight)
+
+print(graph.arregloDeListas)
+#print(graph.getWeight(1,1))
+print("Successors: ")
+print(graph.getSuccessors(4))
