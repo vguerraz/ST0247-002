009da6307b1605318d513c2d4557665397517b36
vguerraz <84991036+vguerraz@users.noreply.github.com>
2022-03-29 10:33:42 -0500

    Update agenteViajero.py

--- laboratorios/lab04/codigo/agenteViajero.py
+++ laboratorios/lab04/codigo/agenteViajero.py
@@ -1,21 +1,64 @@
-def rutas(mañana : list, tarde : list, limite : int, valor : int):
-  horasextra = 0
-  for i in range (0, len(mañana)):
-    tiempo = max(mañana)+min(tarde)
-    horasextra += (tiempo-limite)
-    mañana.remove(max(mañana))
-    tarde.remove(min(tarde))
+from collections import deque
+import math
 
-  valorAPagar = horasextra * valor
-  return valorAPagar
+class GraphAL:
+  def __init__(self, size): 
+    self.siz = size
+    self.arregloDeListas = [0]*size
+    for i in range(0, size):
+      self.arregloDeListas[i]= deque()
+  
+  def addArc(self, vertex, destination, weight):
+    fila = self.arregloDeListas[vertex]
+    arco = (destination,weight)
+    fila.append(arco)
+        
+  def getWeight(self, source, dest)->int:
+    vertex = self.getSuccessors(source)
+    for i in range(0, len(vertex)):
+      if vertex[i][0]==dest:
+        return vertex[i][1]
+    return 0 
+    
+  def getSuccessors(self, vertice):
+    successors = self.arregloDeListas[vertice]
+    return successors
 
+def recorrer(graph : GraphAL):
+  costo = 0
+  elMasCerca = 0
+  recorrido = []
+  recorrido.append(elMasCerca)
+  for vez in range(0,graph.siz):
+    menorEncontrado = math.inf
+    for siguiente in graph.getSuccessors(elMasCerca):
+      peso_actual = siguiente[1]
+      if peso_actual<menorEncontrado and siguiente[0] not in recorrido: 
+        menorEncontrado = peso_actual
+        elMasCerca = siguiente[0]
+    recorrido.append(elMasCerca)
+    costo = costo + menorEncontrado
+    if len(recorrido) == graph.siz:
+      costo = costo + graph.getWeight(recorrido[-1], recorrido[0])
+      print("El recorrido propuesto es: ", recorrido)
+      print("Y tiene un costo de: ",costo)
+      break
 
-def main():
-  l1 = list(map(int,input().split())) #linea 1 de entrada
-  while (len(l1) == 3) and (l1 != [0, 0, 0]):
-    mañana = list(map(int,input().split())) #línea 2, horarios mañana
-    tarde = list(map(int,input().split())) #linea 3, horarios tarde
-    print(rutas(mañana, tarde, l1[1], l1[2]))
-    l1 = list(map(int,input().split())) #linea 1 de entrada
-
-main()
+graph = GraphAL(5)
+graph.addArc(0,1,10)
+graph.addArc(0,3,14)
+graph.addArc(0,4,10)
+graph.addArc(1,0,10)
+graph.addArc(1,2,7)
+graph.addArc(1,3,12)
+graph.addArc(1,4,15)
+graph.addArc(2,1,7)
+graph.addArc(2,3,20)
+graph.addArc(3,0,14)
+graph.addArc(3,1,12)
+graph.addArc(3,2,20)
+graph.addArc(3,4,8)
+graph.addArc(4,0,10)
+graph.addArc(4,1,15)
+graph.addArc(4,3,8)
+recorrer(graph)
